5e6b522d84f70386f8baa1e1a959f8f734af064b
Sorawit Trutsat (Kong) <sorawit.tr@gmail.com>
2022-08-07 11:35:40 +0700

    first commit

---
+++ iTaxClone-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iTaxClone-SwiftUI
+//
+//  Created by Sorawit Trutsat on 7/8/2565 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iTaxClone-SwiftUI/iTaxClone_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTaxClone_SwiftUIApp.swift
+//  iTaxClone-SwiftUI
+//
+//  Created by Sorawit Trutsat on 7/8/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct iTaxClone_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
